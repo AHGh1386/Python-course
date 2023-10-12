5b88f6101a4454e714900b66207dd5d70a989acb
AH <140490490+AHGh1386@users.noreply.github.com>
2023-10-12 10:33:38 +0330

    Create app.py

---
+++ OOP/Inheritance/Multiple Inheritance/app.py
@@ -0,0 +1,21 @@
+# Employee -> Person -> LivingCreature -> Thing
+
+# ======================multiple Inheritance========================
+
+
+class Employee:
+    def greet(self):
+        print("Employee Greet")
+
+
+class Person:
+    def greet(self):
+        print("Person Greet")
+
+
+class Manager(Employee, Person):
+    pass
+
+
+manager = Manager()
+manager.greet()
