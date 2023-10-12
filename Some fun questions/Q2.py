ae6285eb514b604a4017d5b24cf46e3cd086897f
AH <140490490+AHGh1386@users.noreply.github.com>
2023-10-12 09:19:35 +0330

    Create Q2.py

---
+++ Some fun questions/Q2.py
@@ -0,0 +1,10 @@
+# fizz buzz
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizz_Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
