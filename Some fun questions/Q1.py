920df9042d888c014bb05fe1676f1652bcec1ee8
AH <140490490+AHGh1386@users.noreply.github.com>
2023-10-12 09:17:25 +0330

    Update Q1.py

--- Some fun questions/Q1.py
+++ Some fun questions/Q1.py
@@ -1,1 +1,12 @@
-
+#   write a program that calculates average
+def average():
+    summery, counter = 0, 0
+    while True:
+        number = int(input("Enter your Number to Calculate New Average :"))
+        if number != 0:
+            summery += number
+            counter += 1
+            print(f"The Current Average is: {summery//counter}")
+        else:
+            print(f"The Last Average is: {summery//counter}")
+            break
