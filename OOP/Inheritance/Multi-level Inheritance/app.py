8226a1ab675f730b5d16d2ffb5b3c1f8e77371a5
AH <140490490+AHGh1386@users.noreply.github.com>
2023-10-12 10:25:53 +0330

    Create app.py

---
+++ OOP/Inheritance/Multi-level Inheritance/app.py
@@ -0,0 +1,18 @@
+# ================================Multi-level Inheritance=====================
+
+
+# class Animals():
+
+#     def eat(self):
+#         print("eat")
+
+
+# class Birds(Animals):
+#     def fly(self):
+#         print("fly")
+
+
+# class Chickens(Birds):
+#     pass
+
+# Employee -> Person -> LivingCreature -> Thing
