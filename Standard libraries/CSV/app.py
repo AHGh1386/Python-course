5383da511fa2d675ae22c54e2a8624543ae375fd
AH <140490490+AHGh1386@users.noreply.github.com>
2023-10-13 11:51:01 +0330

    Create app.py

---
+++ Standard libraries/CSV/app.py
@@ -0,0 +1,11 @@
+with open("data.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["trns_id", "pro_id", "price"])
+    writer.writerow([1000, 1, 10])
+    writer.writerow([2000, 2, 15])
+
+with open("data.csv", "r") as file:
+    reader = csv.reader(file)
+    # print(list(reader))
+    for row in reader:
+        print(row)
