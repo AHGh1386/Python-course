89b499de5aa9c1c0c9148dd354976e44f6e02670
AH <140490490+AHGh1386@users.noreply.github.com>
2023-10-13 11:53:16 +0330

    Create app.py

---
+++ Standard libraries/Sqlite3/app.py
@@ -0,0 +1,7 @@
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "SELECT * from Movies"
+    cursor = conn.execute(command)
+    # for item in cursor:
+    #     print(item)
+    movies = cursor.fetchall()
+    print(movies)
